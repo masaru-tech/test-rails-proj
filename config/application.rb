7021945f9814e669605bb3888606e532887b35bc
MasaruTech <masaru.tech@gmail.com>
2017-05-17 22:34:06 +0900

    i18n対応

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module DevExam
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{yml}').to_s]
   end
 end
